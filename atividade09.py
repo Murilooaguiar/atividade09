cc19a4f690a91dd53610909a7d3450e7967e0e86
Murilooaguiar <146784737+Murilooaguiar@users.noreply.github.com>
2023-10-16 14:48:09 -0300

    atividade09

---
+++ atividade09.py
@@ -0,0 +1,8 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+numero1= int(input("digite o primeiro numero"))
+numero2=int(input("digite o primeiro numero"))
+
+if numero1>numero2:
+    print(f"numero {numero1} é o maior")
+else:
+    print(f"o numero{numero2} é o maior")
